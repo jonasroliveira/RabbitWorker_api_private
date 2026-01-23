a8e064de1c4206ecd1c7bceee6fffefa14e27c53
Jonas Oliveira <jonasroliveira@gmail.com>
2026-01-23 15:15:46 -0300

    Instalado scalar.

--- Program.cs
+++ Program.cs
@@ -1,12 +1,25 @@
+
+//Jonas da Rosa Oliveira
+//https://www.linkedin.com/in/jonas-da-rosa-oliveira
+
+using RabbitWorkerApi.Infrastructure.Messaging;
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
+
+builder.Services.AddSingleton<RabbitMqProducer>();
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.MapOpenApi(); 
+    app.MapScalarApiReference(options => 
+    {
+        options.WithOpenApiRoutePattern("/openapi/v1.json");
+    });
 }
 
 app.UseHttpsRedirection();
--- Readme.md
+++ Readme.md
@@ -1,3 +1,5 @@
 dotnet new webapi -n RabbitWorker.Api
 
 dotnet add package RabbitMQ.Client
+
+dotnet add package Scalar.AspNetCore
